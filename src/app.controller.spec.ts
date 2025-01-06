7ef2a92463ad97f3bc31f0f3d993f84ac924c44c
Joao Vitor <joao.steixeira@solutis.com.br>
2025-01-06 19:11:57 -0300

    ajuste nos testes do nestjs

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,29 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { vi } from 'vitest';
 
 describe('AppController', () => {
   let appController: AppController;
   let appService: AppService;
 
   beforeEach(async () => {
+   await setup();
+  });
+
+  it('should create a user', () => {
+    appController = new AppController(appService)
+    expect(appController.create()).toBe('Usuário criado');
+  });
+
+  it('should return a list of users', () => {
+    appController = new AppController(appService)
+    expect(appController.findAll()).toBe('Lista de usuários');
+  });
+
+  const setup = async () => {
+    const mockAppService = {
+      createUser: vi.fn().mockReturnValue('Usuário criado'),
+      listUsers: vi.fn().mockReturnValue('Lista de usuários'),
+    };
+
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
       providers: [
         {
           provide: AppService,
-          useValue: {
-            getHello: vi.fn().mockReturnValue('Hello World!'), // Mock
-          },
+          useValue: mockAppService,
         },
       ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
     appService = app.get<AppService>(AppService);
-  });
-
-  it('should return "Hello World!"', () => {
-    expect(appController.getHello()).toBe('Hello World!');
-  });
+	};
 });
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,18 +5,13 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return 'Hello World!';
-  }
-
-  @Post('cats')
+  @Post('user')
   create(): string {
-    return 'Cria novos gatos';
+    return this.appService.createUser();
   }
 
-  @Get('cats')
+  @Get('users')
   findAll(): string {
-    return 'Retorna todos os gatos';
+    return this.appService.listUsers();
   }
 }
